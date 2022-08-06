58df4fe04aae1a9f252ceb3c01a68ad6e361dc2d
Bas Kaal <3askaal@gmail.com>
2022-08-06 22:15:56 +0200

    simplify sidebar button styling

--- src/core/components/Sidebar/Sidebar.tsx
+++ src/core/components/Sidebar/Sidebar.tsx
@@ -17,7 +17,7 @@ export const Sidebar: FC<any> = ({ children, ...props }: any): ReactElement => {
           onClick={() => setIsOpen(!isOpen)}
           data-testid="sidebar-toggle"
         >
-          {isOpen ? <X size="2rem" /> : <Menu size="2rem" />}
+          {isOpen ? <X size={24} /> : <Menu size={24} />}
         </SSidebarToggle>
         <SSidebarContent
           onClick={() => isOpen && setIsOpen(false)}
