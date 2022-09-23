ca971f139ead37a4928934b42577c60cc14a82a9
Bas Kaal <3askaal@gmail.com>
2022-09-24 00:08:27 +0200

    make styled components wrapper fully typesafe

--- src/core/utils/style/styled.tsx
+++ src/core/utils/style/styled.tsx
@@ -1,11 +1,11 @@
 import styledComponents, { StyledComponent } from 'styled-components'
 import { css, CssFunctionReturnType, SystemStyleObject, Theme } from '@styled-system/css'
-import { forOwn, get, mapKeys, wrap } from 'lodash'
+import { forOwn, get, mapValues, wrap } from 'lodash'
 import deepmerge from 'deepmerge'
 
 export type SStyles = SystemStyleObject
-export type SVariants = { [variantKey: string]: SystemStyleObject }
-export type SComponents = { [componentKey: string]: { default?: SystemStyleObject, variants?: SystemStyleObject } }
+export type SVariants = { [variantKey: string]: SStyles }
+export type SComponents = { [componentKey: string]: { default?: SStyles, variants?: SVariants } }
 
 export interface ExpectedProps {
   sRef: string;
@@ -17,19 +17,51 @@ export interface ExpectedProps {
 
 type DeclProps = (SStyles | SVariants | string)
 
-function parseStyles(styles: SystemStyleObject, props: any) {
+// Resolves   CSS object or CSS style function
+function parseStyles(styles: SystemStyleObject, props: ExpectedProps) {
   return (typeof styles === 'function') ? css(styles(props)) : css(styles);
 }
 
-export const mergeStyles = (props: ExpectedProps, ...declProps: DeclProps[]): CssFunctionReturnType[] => {
-  const defaultStyles = declProps[0]
-  const variantStyles = declProps[1]
-  const sRef = declProps[2] || ''
-
-  const { s: inlineProps, ...newProps } = props
-  const themeComponentStyles = get(props?.theme?.components, sRef || newProps.sRef)
-
+export const mergeStyles = (props: ExpectedProps, ...declParams: DeclProps[]): CssFunctionReturnType[] => {
   const styleFunctions: CssFunctionReturnType[] = []
+
+  // TODO: Define types in order in declParams rest param
+  const defaultStyles = declParams[0] as SStyles | undefined
+  const variantStyles = declParams[1] as SVariants | undefined
+  const sRef = declParams[2] as string | undefined
+
+  // Declare inline styles
+  const inlineStyles = props.s
+
+  // Declare component styles defined in theme
+  const themeComponentStyles = get(props?.theme?.components, sRef || props.sRef)
+
+  // Apply default styles
+  if (defaultStyles) {
+    styleFunctions.push(parseStyles(defaultStyles, props))
+  }
+
+  // Apply defaullt styles defined in theme
+  if (themeComponentStyles?.default) {
+    styleFunctions.push(parseStyles(themeComponentStyles.default, props))
+  }
+
+  // Apply variants styling
+  if (variantStyles) {
+    // Merge variants defined in component and in theme
+    const mergedVariants = deepmerge(variantStyles, themeComponentStyles?.variants || {})
+
+    forOwn(mergedVariants, (variantStyle: SystemStyleObject, variantKey: string) => {
+      if (props[variantKey]) {
+        styleFunctions.push(parseStyles(variantStyle, props))
+      }
+    })
+  }
+
+  // Apply inline styling
+  if (inlineStyles) {
+    styleFunctions.push(parseStyles({ '&&&': inlineStyles }, props))
+  }
 
   // TODO: Fix this feature
   // if (newProps.children) {
@@ -39,41 +71,18 @@ export const mergeStyles = (props: ExpectedProps, ...declProps: DeclProps[]): Cs
   //   )
   // }
 
-  if (defaultStyles) {
-    styleFunctions.push(parseStyles(defaultStyles, newProps))
-  }
-
-  if (themeComponentStyles?.default) {
-    styleFunctions.push(parseStyles(themeComponentStyles.default, newProps))
-  }
-
-  if (variantStyles) {
-    const mergedVariants = deepmerge(variantStyles, themeComponentStyles?.variants || {})
-
-    forOwn(mergedVariants, (variantStyle: SystemStyleObject, variantKey: string) => {
-      if (newProps[variantKey]) {
-        styleFunctions.push(parseStyles(variantStyle, newProps))
-      }
-    })
-  }
-
-
-  if (inlineProps) {
-    styleFunctions.push(parseStyles({ '&&&': newProps.s }, newProps))
-  }
-
   return styleFunctions
 }
 
 interface WrappedCallback { (props: ExpectedProps): any; }
 type Wrapped = (cb: WrappedCallback) => StyledComponent<'div', Theme>;
 
-export const s = mapKeys(
+export const s = mapValues(
   styledComponents,
   (value) => {
-    return wrap<Wrapped, DeclProps, any>(value, (func, ...declProps) => {
+    return wrap<Wrapped, DeclProps, StyledComponent<any, Theme>>(value, (func, ...declParams) => {
       return func((props) => {
-        return mergeStyles(props, ...declProps)
+        return mergeStyles(props, ...declParams)
       })
     })
   },
