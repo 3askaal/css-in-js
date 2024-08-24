4a34c378992ee777985a6f9ba902a8ddcb1350ce
Bas Kaal <3askaal@gmail.com>
2024-08-24 13:06:50 +0200

    fix issues with css injection and boost performance, fix issues with re-rendering components

--- src/inject.ts
+++ src/inject.ts
@@ -1,12 +1,43 @@
-let tag
+let tags = []
+let ruleCount = 0
+
+const createTag = () => {
+  let tag = document.createElement('style')
+  tag.appendChild(document.createTextNode(''))
+  return tag
+}
+
+const insertTag = () => {
+  const tag = createTag()
+  document.head.appendChild(tag)
+  tags.push(tag)
+  ruleCount = 0
+}
 
 export const inject = (cssString: string): void => {
   if (typeof window === 'undefined') return
+  if (ruleCount % 50000 === 0) insertTag()
+  
+  insertRules(cssString)
+}
 
-  if (!tag) {
-    tag = window.document.createElement('style')
-    window.document.head.appendChild(tag)
+const insertRules = (cssString: string) => {
+  const cssRules = cssString.split(/}\n/)
+
+  for (let i = 0; i < cssRules.length; i++) {
+    const cssRule = cssRules[i]
+    if (cssRule) insertRule(cssRules[i])
+  }
+}
+
+const insertRule = (cssRule: string) => {
+  const tag = tags[tags.length - 1]
+
+  try {
+    tag.sheet?.insertRule(cssRule, tag.sheet?.cssRules.length)
+  } catch (err) {
+    console.log(err)
   }
 
-  tag.textContent += cssString
+  ruleCount++
 }
